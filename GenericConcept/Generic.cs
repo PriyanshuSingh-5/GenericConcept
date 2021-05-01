8312548780d224800b208f536f5754b122805bb1
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-01 14:41:19 +0530

    [Priyanshu]modified generic extended method

--- GenericConcept/Generic.cs
+++ GenericConcept/Generic.cs
@@ -6,41 +6,41 @@ using System.Threading.Tasks;
 
 namespace GenericConcept
 {
-    
-        public class Generic<T>
-        {
-            public T[] value;
-            public Generic(T[] value)
-            {
-                this.value = value;
-            }
-        public T[] Sort(T[] values)
-        {
-            Array.Sort(values);
-            return values;
-        }
-        void add(params int[] a) //specify a method parameter
-        {
+    public class Generic<T> where T : IComparable
+    {
 
-        }
-        public T MaxValue(params T[] values)
-        {
-            var sorted_values = Sort(values);
+      public T[] value;
+    public Generic(T[] value)
+    {
+        this.value = value;
+    }
 
-            return sorted_values[sorted_values.Length - 1];
-        }
+    public T[] Sort(T[] values) //sort 
+    {
+        Array.Sort(values);
+        return values;
+    }
+    void add(params int[] a)
+    {
 
-        public T MaxMethod() //To get max value
-        {
-            var max = MaxValue(this.value);
-            return max;
-        }
+    }
+    public T MaxValue(params T[] values) //parameters
+    {
+        var sorted_values = Sort(values);
 
-        public void PrintMaxValue()
-        {
-            var max = MaxValue(this.value);
-            Console.WriteLine("Maximum value is " + max); //print max value
-        }
+        return sorted_values[sorted_values.Length - 1];
+    }
+
+    public T MaxMethod()
+    {
+        var max = MaxValue(this.value);
+        return max;
+    }
+
+    public void PrintMaxValue()
+    {
+        var max = MaxValue(this.value);
+        Console.WriteLine("Maximum value is " + max);
     }
 }
-        
+}
